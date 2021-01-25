b313c8a57346e3912d990fddfabf53a75808e689
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-25 03:50:17 +0300

    preparation

---
+++ src/main/java/problems/volume001/The_3n_plus_1_problem/the_3n_plus_1_problem.py
@@ -0,0 +1,5 @@
+from src.test.java.problems.volume001.The_3n_plus_1_problem.test_the_3n_plus_1_problem import test_the_3n_plus_1_problem
+
+
+def the_3n_plus_1_problem(i: int, j: int) -> int:
+    return 1
