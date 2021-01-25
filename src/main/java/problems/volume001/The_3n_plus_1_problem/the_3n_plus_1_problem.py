11c071925e0687ab1c8024dadd076a94975e5d7a
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-25 04:57:42 +0300

    preparaion; fixes

--- src/main/java/problems/volume001/The_3n_plus_1_problem/the_3n_plus_1_problem.py
+++ src/main/java/problems/volume001/The_3n_plus_1_problem/the_3n_plus_1_problem.py
@@ -1,5 +1,18 @@
-from src.test.java.problems.volume001.The_3n_plus_1_problem.test_the_3n_plus_1_problem import test_the_3n_plus_1_problem
+def calculate_cycle_length(number: int) -> int:
+    result = []
+    while number > 1:
+        if number % 2 == 1:
+            number = number * 3 + 1
+            result.append(number)
+        else:
+            number /= 2
+            result.append(number)
+    result.append(number)
+    print(result)
+    return len(result)
 
 
 def the_3n_plus_1_problem(i: int, j: int) -> int:
-    return 1
+    begin = i if i < j else j
+    end = j if i < j else i
+    return max([calculate_cycle_length(x) for x in range(begin, end)])
